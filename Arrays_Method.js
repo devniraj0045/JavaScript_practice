8b6319d37af9d77225bcc278ea29aa7c3ecde4cc
devniraj2003 <devniraj2003@gmail.com>
2023-08-07 17:29:30 +0545

    table

--- Arrays_Method.js
+++ Arrays_Method.js
@@ -1,12 +1,12 @@
 //Array Methods
-let num =[1,2,3,35,4]
-let b =num.toString()
-console.log(b)
-let c= num.join("_")
-console.log(c,  typeof c) 
+var num =[1,2,3,35,4]
+// let b =num.toString()
+// console.log(b)
+// let c= num.join("_")
+// console.log(c,  typeof c) 
 
-// num.pop()  this code is use to remove last element
-// console.log(num)
+num.pop()  //this code is use to remove last element
+document.write(num)
 
-let r = num.pop()//  this code is used return the last elements
-console.log(num, r)
+// let r = num.pop()//  this code is used return the last elements
+// console.log(num, r)
